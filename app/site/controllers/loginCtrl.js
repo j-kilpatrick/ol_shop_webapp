bb5a550413185eeda2591d47833ffc197fdf0dc6
Dan <demaria.daniel@gmail.com>
2016-02-10 14:26:19 -0500

    update controllers

--- app/site/controllers/loginCtrl.js
+++ app/site/controllers/loginCtrl.js
@@ -1,5 +1,9 @@
 app.controller('loginCtrl',loginCtrl);
 
-function loginCtrl(productSrv) {
+function loginCtrl(productSrv, $state) {
 	console.log("Login control!");
+	var ctrl = this;
+	ctrl.$state = $state;
+	ctrl.productSrv = productSrv;
+	ctrl.products = products;
 };
