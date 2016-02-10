b6f288e92424e0d15be1b829b1ac51d44224460c
ecarriere <eduardcarriere@gmail.com>
2016-02-10 17:14:49 -0500

    number 11

--- app/site/controllers/adminCtrl.js
+++ app/site/controllers/adminCtrl.js
@@ -5,5 +5,39 @@ function adminCtrl(productSrv, $state) {
 	var ctrl = this;
 	ctrl.$state = $state;
 	ctrl.productSrv = productSrv;
-	ctrl.products = products;
-};
+//	ctrl.products = products;
+
+
+	//Login check (later)
+
+
+
+
+	//watch for updates to products object
+
+	// $scope.$watch(function(){
+ //    	return productSrv.products;
+	// }, function (newValue) {
+	// 	if(productSrv.products.length > 0){
+	// 	    ctrl.products = productSrv.products;
+	// 	    ctrl.is_products = true;
+	// 	}
+	// });
+
+	ctrl.editProduct = function(product){
+	
+	ctrl.$state.go('admin.edit_product',{productId:product.id});
+	
+	}
+
+	ctrl.logout = function(){
+	
+
+	localStorage.removeItem('authToken');
+	ctrl.$state.go('auth');
+
+	}
+
+
+}
+
