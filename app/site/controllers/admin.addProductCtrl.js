4643af31786c244a5f926b2c557ff0ab08d9b519
ecarriere <eduardcarriere@gmail.com>
2016-02-12 11:01:29 -0500

    fridaymorning

--- app/site/controllers/admin.addProductCtrl.js
+++ app/site/controllers/admin.addProductCtrl.js
@@ -3,6 +3,7 @@ app.controller('addProductCtrl',addProductCtrl);
 function addProductCtrl($stateParams,api,productSrv){
 	var ctrl = this
 	ctrl.productSrv = productSrv;
+	ctrl.products = productSrv.products;
 
 	ctrl.categories = [
 		{label:'Cat1',value:'cat1'},
@@ -21,56 +22,58 @@ function addProductCtrl($stateParams,api,productSrv){
 		.then(function(res){
 			console.log(res);
 			ctrl.product = res.data.product;
+
 			//TODO #2 set category based on edit form based on 
 			//product category
 			
 			for(var category in ctrl.categories){
 			if(ctrl.product.category == ctrl.category[category].value){
 
-			// 	}
-			// }
-		})
+		}
 	}
 
 
-}
+})
 }
 }
 
-ProductCtrl.prototype.addProduct = function(){
+addProductCtrl.prototype.addProduct = function(){
 	var ctrl = this;
-	//TODO #2
-	//create product object, pass to plugin service
-	//Update text in button
+	
+	console.log("product add function works")	
+	
 	var product = {
 		name: ctrl.name,
-		description: ctrl.description,
-		image: ctrl.image,
 		category: ctrl.category,
+		image: ctrl.image,
 		price: ctrl.price,
+		description: ctrl.description,
 		status: true,
 	}
 
+	console.log(product)
 	ctrl.productSrv.addProduct(product);
 
 }
 
-ProductCtrl.prototype.updateProduct = function(){
-	var ctrl = this;
+// addProductCtrl.prototype.updateProduct = function(){
+// 	var ctrl = this;
 
-	//TODO #2
-	//create product object, pass to plugin service
-	//Update text in button
-	ctrl.product_update_btn = "updating";
-	ctrl.productSrv.updateProduct(ctrl.product, ctrl.product.id);
-}
+// 	//TODO #2
+// 	//create product object, pass to plugin service
+// 	//Update text in button
 
-ProductCtrl.prototype.deleteProduct = function(){
-	var ctrl = this;
+// 	ctrl.product_update_btn = "updating";
+// 	ctrl.productSrv.updateProduct(ctrl.product, ctrl.product.id);
+// }
 
-	//TODO #2
-	//remove product, pass to plugin service
-	//update text in button
-	ctrl.product.product_delete_btn = "Deleting";
-	ctrl.productSrv.deleteProduct(ctrl.product.id);
-}
+// addProductCtrl.prototype.deleteProduct = function(){
+// 	var ctrl = this;
+
+// 	//TODO #2
+// 	//remove product, pass to plugin service
+// 	//update text in button
+
+// 	ctrl.product.product_delete_btn = "Deleting";
+// 	ctrl.productSrv.deleteProduct(ctrl.product.id);
+// }
